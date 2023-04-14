f9d09f9bfc2b8256466f185abd2e3130f5470ce7
atomicapple <brianzhangaa@gmail.com>
2023-04-14 08:36:46 -0400

    timer

--- cvcar/my_cv_pilot.py
+++ cvcar/my_cv_pilot.py
@@ -3,6 +3,7 @@ import numpy as np
 import logging
 from cv import *
 from threshold import *
+from time import time
 
 class MockCvPilot:
     def __init__(self, pid, cfg):
@@ -12,9 +13,13 @@ class MockCvPilot:
         if cam_img is None:
             return 0, 0, None
 
+        start = time()
         steering, throttle = 0, 0
         # img, steering, throttle = process_img(cam_img)
         img = threshold(cam_img)
+
+        end = time()
+        print(f"done in {end-start} sec")
 
         return steering, throttle, img
 
